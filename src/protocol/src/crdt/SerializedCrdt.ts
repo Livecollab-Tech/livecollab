d2359c00f0b43793d18f7197f4df75e2e5546db6
0x303133 <0x303133@proton.me>
2024-12-02 20:26:11 +0300

    feat(protocol): Create Serialized Crdt types

--- src/protocol/src/crdt/SerializedCrdt.ts
+++ src/protocol/src/crdt/SerializedCrdt.ts
@@ -1,3 +1,7 @@
+import { Json, JsonObject } from "../Json";
+
+export type IdTuple<T> = [id: string, value: T];
+
 /**
  * This enum represents all `CRDT` types, that
  * we will use in `@livecollab/crdt`.
@@ -17,3 +21,60 @@ export enum CrdtType {
   REGISTER = 8,
 }
 
+export type SerializedCrdt = SerializedRootObject | SerializedChild;
+
+export type SerializedChild =
+  | SerializedObject
+  | SerializedMap
+  | SerializedList
+  | SerializedText
+  | SerializedRegister;
+
+export type SerializedRootObject = {
+  readonly type: CrdtType.OBJECT;
+  readonly data: JsonObject;
+
+  /* Root objects don't have a parent */
+  readonly parentId?: never;
+  readonly parentKey?: never;
+};
+
+export type SerializedObject = {
+  readonly type: CrdtType.OBJECT;
+  readonly parentId: string;
+  readonly parentKey: string;
+  readonly data: JsonObject;
+};
+
+export type SerializedMap = {
+  readonly type: CrdtType.MAP;
+  readonly parentId: string;
+  readonly parentKey: string;
+};
+
+export type SerializedList = {
+  readonly type: CrdtType.LIST;
+  readonly parentId: string;
+  readonly parentKey: string;
+};
+
+export type SerializedText = {
+  readonly type: CrdtType.TEXT;
+  readonly parentId: string;
+  readonly parentKey: string;
+};
+
+export type SerializedRegister = {
+  readonly type: CrdtType.REGISTER;
+  readonly parentId: string;
+  readonly parentKey: string;
+  readonly data: Json;
+};
+
+export const isRootCrdt = (crdt: SerializedCrdt): crdt is SerializedRootObject => {
+  return crdt.type === CrdtType.OBJECT && !isChildCrdt(crdt);
+}
+
+export const isChildCrdt = (crdt: SerializedCrdt): crdt is SerializedChild => {
+  return crdt.parentId !== undefined && crdt.parentKey !== undefined;
+}
