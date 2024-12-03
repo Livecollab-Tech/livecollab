58bbd999b697004ad8042b6a9be4bb987db57925
0x303133 <0x303133@proton.me>
2024-12-03 15:07:53 +0300

    feat(protocol): Change Register value in CrdtTypes enum

--- src/protocol/src/crdt/SerializedCrdt.ts
+++ src/protocol/src/crdt/SerializedCrdt.ts
@@ -5,20 +5,13 @@ export type IdTuple<T> = [id: string, value: T];
 /**
  * This enum represents all `CRDT` types, that
  * we will use in `@livecollab/crdt`.
- *
- * `CrdtType.REGISTER` use `8` because it's last
- * value that could be in `CrdtType`, but in the
- * future, we can add some `CrdtTypes` to package.
- * That's why we use exactly this value in order
- * to be backward compatible.
  */
 export enum CrdtType {
-  OBJECT = 0,
-  MAP = 1,
-  LIST = 2,
-  TEXT = 3,
-
-  REGISTER = 8,
+  REGISTER = 0,
+  OBJECT = 1,
+  MAP = 2,
+  LIST = 3,
+  TEXT = 4,
 }
 
 export type SerializedCrdt = SerializedRootObject | SerializedChild;
