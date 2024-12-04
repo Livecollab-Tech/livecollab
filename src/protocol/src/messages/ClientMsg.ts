2a43890c08f85704930397c9d3a454d2bd8a1b3e
0x303133 <0x303133@proton.me>
2024-12-04 19:07:27 +0300

    feat(protocol): Create client messages

--- src/protocol/src/messages/ClientMsg.ts
+++ src/protocol/src/messages/ClientMsg.ts
@@ -1,3 +1,6 @@
+import { Json, JsonObject } from "../Json";
+import { Op } from "../crdt/Op";
+
 export enum ClientMsgCode {
   /* For Broadcast & Presence */
   BROADCAST_EVENT = 200,
@@ -6,3 +9,35 @@ export enum ClientMsgCode {
   FETCH_STORAGE = 300,
   UPDATE_STORAGE = 301,
 }
+
+/**
+ * Messages that can be sent from the client to the server.
+ */
+export type ClientMsg<P extends JsonObject, E extends Json> =
+  /* For Broadcast & Presence */
+  | ClientBroadcastEventMsg<E>
+  | ClientUpdatePresenceMsg<P>
+
+  /* For storage */
+  | ClientUpdateStorageMsg
+  | ClientFetchStorageMsg
+
+export type ClientBroadcastEventMsg<E extends Json> = {
+  readonly type: ClientMsgCode.BROADCAST_EVENT;
+  readonly event: E;
+};
+
+export type ClientUpdatePresenceMsg<P extends JsonObject> = {
+  readonly type: ClientMsgCode.UPDATE_PRESENCE;
+  readonly isFull: boolean;
+  readonly data: P;
+};
+
+export type ClientFetchStorageMsg = {
+  readonly type: ClientMsgCode.FETCH_STORAGE;
+};
+
+export type ClientUpdateStorageMsg = {
+  readonly type: ClientMsgCode.UPDATE_STORAGE;
+  readonly ops: Op[];
+};
